9063b142f1525a478f8473614c025baf1f2ceb55
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:33 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -10,3 +10,31 @@
 ## f,110
 ## g,35
 ##
+### Punto 12
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = []
+
+i = 0
+for elemt in BD:
+	data.append([])
+	for e in elemt:
+		a = e.split(',')
+		if(len(a) == 1):
+			data[i].append(a[0])
+		else:
+			data[i].append(a)
+	i += 1
+
+result = {}
+
+for element in data:
+	for key in element[3]:
+		if key in result:
+			result[key] += int(element[1])
+		else:
+			result[key] = int(element[1])
+
+for key in sorted(result.keys()):
+     print(key + ',' + str(result[key]))
