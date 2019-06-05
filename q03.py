e722dc43581819eab6537e40cae6ca3f5ad7baf3
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:14 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,18 @@
 ## D,23
 ## E,67
 ##
+### Punto 3
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = BD
+result = {}
+
+for elemt in data:
+	result[elemt[0]] = 0
+
+for elemt in data:
+	result[elemt[0]] = result[elemt[0]] + int(elemt[1])
+
+for key in sorted(result.keys()):  
+     print(key + ',' + str(result[key]))
