1d8ec38bd6b588b333e79e57096317b42289fc76
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:19 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -13,3 +13,34 @@
 ## iii,2,7
 ## jjj,2,5
 ##
+### Punto 6
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = []
+
+i = 0
+for elemt in BD:
+	data.append([])
+	for e in elemt:
+		a = e.split(',')
+		if(len(a) == 1):
+			data[i].append(a[0])
+		else:
+			data[i].append(a)
+	i += 1
+
+result = {}
+
+for element in data:
+	for el in element[4]:
+		aux = el.split(':')
+		result[aux[0]] = []
+
+for element in data:
+	for el in element[4]:
+		aux = el.split(':')
+		result[aux[0]].append(int(aux[1]))
+
+for key in sorted(result.keys()):  
+     print(key + ',' + str(min(result[key])) + ',' + str(max(result[key])))
