ad015b4754d6673aa7f1d1fa7e9d50d965e2cf5b
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:17 -0500

    Update q05.py

--- q05.py
+++ q05.py
@@ -7,3 +7,18 @@
 ## D,7,1
 ## E,9,1
 ##
+### Punto 5
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = BD
+result = {}
+
+for elemt in data:
+	result[elemt[0]] = []
+
+for elemt in data:
+	result[elemt[0]].append(int(elemt[1]))
+
+for key in sorted(result.keys()):  
+     print(key + ',' + str(max(result[key])) + ',' + str(min(result[key])))
