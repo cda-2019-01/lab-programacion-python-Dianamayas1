7a9f04b5df7bb44edbc2bc07d3b239d763c5752d
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:10 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,13 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+### Punto 1
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = BD
+
+suma_col = 0
+for i in data:
+    suma_col += int(i[1])
+print(suma_col)
