1c927ab9671119add6ed264d27036ce96a1ce251
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:12 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,19 @@
 ## D,6
 ## E,14
 ##
+### Punto 2
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = BD
+
+result = {}
+
+for elemt in data:
+	result[elemt[0]] = 0
+
+for element in data:
+	result[elemt[0]] = result[elemt[0]] + 1
+
+for key in sorted(result.keys()):  
+     print(key + ',' + str(result[key]))
