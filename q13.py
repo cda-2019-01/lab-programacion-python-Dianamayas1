91c3360ae8967c579a10f8d3c108d26caf4e550c
Diana Maya <dmmayas@unal.edu.co>
2019-06-05 03:30:35 -0500

    Update q13.py

--- q13.py
+++ q13.py
@@ -10,3 +10,25 @@
 ## E,11
 ## E,16
 ##
+### Punto 13
+BD = open('data.csv', 'r').readlines()
+BD = [row[0:-1] for row in BD]
+BD = [row.split('\t') for row in BD]
+data = []
+
+i = 0
+for elemt in BD:
+	data.append([])
+	for e in elemt:
+		a = e.split(',')
+		if(len(a) == 1):
+			data[i].append(a[0])
+		else:
+			data[i].append(a)
+	i += 1
+
+for element in data:
+	aux = 0
+	for e in element[4]:
+		aux += int(e.split(':')[1])
+	print(element[0] + ',' + str(aux))
